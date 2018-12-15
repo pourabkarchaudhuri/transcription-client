84b5eeab4550c0c5754b68801b70ef3feb6e8a82
Pourab Karchaudhuri <36023@Hexaware.com>
2018-12-16 02:52:53 +0530

    Updated UI, Sockets and Error removed

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,14 +1,14 @@
 var socket;
-
+console.log("Declaring global socket config")
 function handshake() {
-        window.console.log("Handshaking....")
+        window.console.log("Handshaking ....");
         socket = io.connect('http://ec2-13-232-119-51.ap-south-1.compute.amazonaws.com:3000');
-
 };
 
 
 function call(val){
     window.console.log("Calling backend with :::::" + val)  
+    
     socket.emit('chat', {
         message: val,
         handle: "transcriber"
